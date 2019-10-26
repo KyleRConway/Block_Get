9174a084a034cedde891251bb13085fbc6b6a6ad
Izacistan <hillakerisaac@gmail.com>
2019-10-25 20:35:48 -0500

    Drew all game objects to screen. Next step is to make 'player' object moveable.

---
+++ Block_Get.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init() # this always needs to be called after importing pygame.
+Display_Window = pygame.display.set_mode((400, 300)) # sets up the main display window for the game.
+pygame.display.set_caption('Block Get') # sets up caption on the top-left of the game window
+
+#Setup Colors
+green = (0, 128, 0)
+blue = (0, 0, 255)
+gray =  (128, 128, 128)
+yellow = (255, 255, 0)
+#Draw game Characters and objects
+player = pygame.draw.rect(Display_Window, green, (200, 150, 25, 25))
+treasure = pygame.draw.rect(Display_Window, blue, (350, 200, 20, 20))
+goal = pygame.draw.rect(Display_Window, yellow, (85, 200, 20, 20))
+
+#Setup Walls
+wall = pygame.draw.rect(Display_Window, gray, (150, 75, 25, 25))
+wall_l_long_top = pygame.draw.rect(Display_Window, gray, (0, 0, 400, 25)) # (x, y, length, width)
+wall_l_long_bottom = pygame.draw.rect(Display_Window, gray, (0, 275, 400, 25))
+wall_l_long_left = pygame.draw.rect(Display_Window, gray, (0, 0, 25, 275))
+wall_l_long_right = pygame.draw.rect(Display_Window, gray, (375, 0, 25, 275))
+
+
+
+while True: # PRIMARY GAME LOOP. The loop's condition is "True", but the loop will always evaluate to "False" which always keeps the loop repeating. Only a break statement will end the loop.
+    #This "Game Loop" 1. Handles Events, 2: Updates the gate state, 3: Draws the game state to the screen.
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
